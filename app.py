5793d9252b5fd38846a763c607e027da888c72ec
Sachkov Ilya <sos31337@gmail.com>
2023-02-12 02:16:15 +0300

    12

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    code = request.args.get('code')
+    state = request.args.get('state')
+    return code, state
+
+
+if __name__ == '__main__':
+    app.run()
