25a882f357fcfb23f6a3d45e2e1fa4181fd50b3a
Sachkov Ilya <sos31337@gmail.com>
2023-02-12 02:51:19 +0300

    12

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     if request.args:
         code = request.args.get('code')
