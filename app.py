8a04b523558712c63d2e360f707dc018dc6096ba
Sachkov Ilya <sos31337@gmail.com>
2023-02-16 01:50:24 +0300

    12

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, redirect, session, url_for
 from requests_oauthlib import OAuth2Session
 import dotenv
 import os
+import base64
+import re
+import hashlib
 
 
 application = Flask(__name__)
@@ -14,21 +17,26 @@ token_url = 'https://api.etsy.com/v3/public/oauth/token'
 scope = 'transactions_r%20transactions_w'
 redirect_uri = 'https://flask-etsyapi-production.up.railway.app/callback'
 code_challenge_method = 'S256'
-code_challenge = 'DSWlW2Abh-cf8CeLL8-g3hQ2WQyYdKyiu83u_s7nRhI'
-
+code_verifier = base64.urlsafe_b64encode(os.urandom(32)).decode("utf-8")
+code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
+code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
+code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
+code_challenge = code_challenge.replace("=", "")
 
 
 
 @application.route('/')
 def index():
     etsy = OAuth2Session(
+        client_id,
         redirect_uri=redirect_uri,
         scope=scope,
-        client_id=client_id,
+    )
+    auth_url, state = etsy.authorization_url(
+        auth_base_url,
         code_challenge=code_challenge,
         code_challenge_method=code_challenge_method,
     )
-    auth_url, state = etsy.authorization_url(auth_base_url)
     session['oauth_state'] = state
     return redirect(auth_url)
 
